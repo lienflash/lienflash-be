b494c674c1546e4fe3d7f31003ff83411ce19f1d
Nick Edwin <nickedwin85@gmail.com>
2020-10-22 15:31:16 -0600

    Initial creation of jobs table

---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_22_212049) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "job_street"
+    t.string "job_city"
+    t.string "job_state"
+    t.string "job_zip"
+    t.string "company_name"
+    t.string "contact_name"
+    t.datetime "date_of_completion"
+    t.float "material_cost"
+    t.float "labor_cost"
+    t.text "job_description"
+    t.string "job_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
