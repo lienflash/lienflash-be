5d4971bf006c103fa360ddd524671c44d7e93511
Nick Edwin <nickedwin85@gmail.com>
2020-10-24 15:01:03 -0600

    Created email template for new job creation

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -22,15 +22,20 @@ class Api::V1::JobsController < ApplicationController
 
       from = Email.new(email: 'josh.tukman@gmail.com')
       to = Email.new(email: 'nickedwin85@gmail.com')
-      subject = 'Sending with SendGrid is Fun'
-      content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-      mail = Mail.new(from, subject, to, content)
+      subject = 'A new job has been created with Lien Flash'
+      content = Content.new(type: 'text/plain', value:
+        "A new job located at: #{job.job_street} #{job.job_city} #{job.job_state}, #{job.job_zip} has been created using Lien Flash.
+This job has an unpaid balance of $#{job.material_cost + job.labor_cost}.
+In 60 days you will be given the option to create a Letter of Intent to File a Lien.
+
+Thank you for using Lien Flash!
+
+Please do not reply to this email. This is a post-only, outbound email. We will be unable to respond to your reply.
+Should you have any questions please reach out to us at: info@lienflash.com
+© 2020 Lien Flash. All Rights Reserved.")
+      mail = SendGrid::Mail.new(from, subject, to, content)
       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
       response = sg.client.mail._('send').post(request_body: mail.to_json)
-      puts response.status_code
-      puts response.body
-      puts response.headers
-
     else
       render json: {"data":{"errors": job.errors.full_messages}}, status: 400
     end
