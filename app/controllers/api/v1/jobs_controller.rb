5346271c799336a51fa3867d4809b8a181766228
Josh Tukman <josh.tukman@gmail.com>
2020-10-26 17:42:27 -0600

    Pull welcome email out of controller into worker file

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,7 +1,5 @@
 require 'sendgrid-ruby'
 require 'twilio-ruby'
-
-
 include SendGrid
 
 class Api::V1::JobsController < ApplicationController
@@ -20,23 +18,7 @@ class Api::V1::JobsController < ApplicationController
     if job.save
       render json: JobSerializer.new(job), status: 201
 
-#       from = Email.new(email: 'josh.tukman@gmail.com')
-#       to = Email.new(email: 'leland.white@gmail.com')
-#       subject = 'A new job has been created with Lien Flash'
-#       content = Content.new(type: 'text/plain', value:
-#         "A new job located at: #{job.job_street} #{job.job_city} #{job.job_state}, #{job.job_zip} has been created using Lien Flash.
-# This job has an unpaid balance of $#{job.material_cost + job.labor_cost}.
-# In 60 days you will be given the option to create a Letter of Intent to File a Lien.
-
-# Thank you for using Lien Flash!
-
-# Please do not reply to this email. This is a post-only, outbound email. We will be unable to respond to your reply.
-# Should you have any questions please reach out to us at: info@lienflash.com
-# © 2020 Lien Flash. All Rights Reserved.")
-#       mail = SendGrid::Mail.new(from, subject, to, content)
-#       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-#       response = sg.client.mail._('send').post(request_body: mail.to_json)
-
+      WelcomeWorker.new.welcome_email(job)
 
 # account_sid = ENV['TWILIO_SID']
 # auth_token = ENV['TWILIO_AUTH_TOKEN']
