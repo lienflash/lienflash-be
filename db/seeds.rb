157dc1e708c1c6f13a30d3cf261522acd0c4e52d
Josh Tukman <josh.tukman@gmail.com>
2020-10-26 15:29:35 -0600

    Add 2 jobs to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+job1 = Job.create(
+        job_street: "123 Main St.",
+        job_city: "Denver",
+        job_state: "Co",
+        job_zip: "80218",
+        date_of_completion: "2020-01-01T04:05:06+00:00",
+        company_name: "Construction Co Inc LLC",
+        contact_name: "Timmy Tim Tim",
+        material_cost: 1000.00,
+        labor_cost: 100.00,
+        job_description: "We fixed some stuff.",
+        job_id: "ABC123"
+      )
+job2 = Job.create(
+        job_street: "2nd Location St.",
+        job_city: "Boulder",
+        job_state: "Co",
+        job_zip: "80309",
+        date_of_completion: "2020-10-20T04:05:06+00:00",
+        company_name: "Construction Co Inc LLC",
+        contact_name: "Josh T",
+        material_cost: 1000.00,
+        labor_cost: 100.00,
+        job_description: "We fixed some different stuff.",
+        job_id: "ABC124"
+      )
