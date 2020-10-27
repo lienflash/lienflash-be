467f3a7184d839d7864cabff180786cec576bd6f
Nick Edwin <nickedwin85@gmail.com>
2020-10-27 13:19:53 -0600

    Email send using sidekiq for job creation

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,12 +1,11 @@
 require 'sendgrid-ruby'
 require 'twilio-ruby'
-
 include SendGrid
 
 class Api::V1::JobsController < ApplicationController
 
   def index
-    render json: JobSerializer.new(Job.all) 
+    render json: JobSerializer.new(Job.all)
   end
 
   def show
@@ -15,28 +14,10 @@ class Api::V1::JobsController < ApplicationController
 
   def create
     job = Job.new(job_params)
-
     if job.save
       render json: JobSerializer.new(job), status: 201
-
-#       from = Email.new(email: 'josh.tukman@gmail.com')
-#       to = Email.new(email: 'nickedwin85@gmail.com')
-#       subject = 'A new job has been created with Lien Flash'
-#       content = Content.new(type: 'text/plain', value:
-#         "A new job located at: #{job.job_street} #{job.job_city} #{job.job_state}, #{job.job_zip} has been created using Lien Flash.
-# This job has an unpaid balance of $#{job.material_cost + job.labor_cost}.
-# In 60 days you will be given the option to create a Letter of Intent to File a Lien.
-
-# Thank you for using Lien Flash!
-
-# Please do not reply to this email. This is a post-only, outbound email. We will be unable to respond to your reply.
-# Should you have any questions please reach out to us at: info@lienflash.com
-# © 2020 Lien Flash. All Rights Reserved.")
-#       mail = SendGrid::Mail.new(from, subject, to, content)
-#       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-#       response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-
+      id = job.id
+      WelcomeEmailWorker.perform_in(2.mintues.from_now, id, 2)
     else
       render json: {"data":{"errors": job.errors.full_messages}}, status: 400
     end
