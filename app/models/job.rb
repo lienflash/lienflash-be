858cbdb988a3cd244950b3e4a4e6e428ebafaa18
Nick Edwin <nickedwin85@gmail.com>
2020-10-27 13:23:25 -0600

    Added staus enum and class methods for status update if late

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Job < ApplicationRecord
 
   validates :job_street, presence: true
@@ -5,6 +6,23 @@ class Job < ApplicationRecord
   validates :job_state, presence: true
   validates :job_zip, presence: true
   validates :date_of_completion, presence: true
+  enum status: { "good standing": 0, "NOI Eligible": 1, "NOI filed": 2, "Lien Filed": 3, "inactive": 4}
+
+  def days_outstanding
+    today = Date.today
+    date_time = DateTime.parse("#{self.date_of_completion}")
+    (today - date_time).to_i
+  end
+
+  def late?
+    days_outstanding > 30
+  end
+
+  def status_update
+    if self.late?
+      self.status = 1
+    end
+  end
   # validates :company_name
   # validates :contact_name
   # validates :material_cost
