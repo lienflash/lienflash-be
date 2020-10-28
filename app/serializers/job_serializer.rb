0b90981a9464c3bca29258264bf96fc06eae7fed
Nick Edwin <nickedwin85@gmail.com>
2020-10-28 15:06:51 -0600

    Serializer update

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :job_street, :job_city, :job_state, :job_zip, :date_of_completion, :company_name, :contact_name, :material_cost, :labor_cost, :job_description, :job_id, :status
+  attributes :job_type, :job_site_contact_name, :job_site_name, :job_site_address, :job_site_address_line_2, :job_site_city, :job_site_state, :job_site_zip_code, :completion_date, :description_of_work, :labor_cost, :material_cost, :total_cost, :client_company_name, :business_address, :business_address_line_2, :business_city, :business_state, :business_zip_code, :additional_info, :status
 end
