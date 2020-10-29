cc92c11cae6be8189bb3a61699555e162161dd96
Josh Tukman <josh.tukman@gmail.com>
2020-10-29 14:37:41 -0600

    Add new text  notification methods

--- app/mailers/customer_text.rb
+++ app/mailers/customer_text.rb
@@ -15,4 +15,32 @@ class CustomerText
     body: "A new job has been created using Lien Flash."
     )
   end
+
+  def job_text_notification
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+14159413974' # Your Twilio number
+    to = '+17203192655' # Your mobile phone number
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "A job in Lien Flash requires your attention.")
+  end
+
+  def final_text_notification
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+14159413974' # Your Twilio number
+    to = '+17203192655' # Your mobile phone number
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "A job in Lien Flash desperately requires your attention.")
+  end
 end
