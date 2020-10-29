12ca9faf98b8098109e2aa94e20975f27c7a15c6
Nick Edwin <nickedwin85@gmail.com>
2020-10-29 16:08:17 -0600

    Updated tests to make them pass

--- spec/facades/job_facade_spec.rb
+++ spec/facades/job_facade_spec.rb
@@ -9,17 +9,16 @@ RSpec.describe  "Rake Tasks" do
       job2 = create(:job, completion_date: 1.days.ago)
       job3 = create(:job, status: "NOI filed", completion_date: 44.days.ago)
       jobs = [job1, job2, job3]
-      # JobFacade.update_jobs
+      
       expect(job1.status).to eq("good standing")
       expect(job2.status).to eq("good standing")
       expect(job3.status).to eq("NOI filed")
       travel(1.days)
-      # binding.pry
-      JobFacade.new.update_jobs(jobs)
-      # jobs = Job.all
 
-      # job1 = Job.first
-      binding.pry
+      JobFacade.new.update_jobs
+
+      job1 = Job.first
+
       expect(job1.status).to eq("NOI Eligible")
       expect(job2.status).to eq("good standing")
       expect(job3.status).to eq("NOI filed")
