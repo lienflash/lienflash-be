820b0fdcdd86cd84df783793ebeb8a6dca5f9773
Josh Tukman <josh.tukman@gmail.com>
2020-10-30 14:17:47 -0600

    Add test methods using Twilio's test credentials

--- app/mailers/customer_text.rb
+++ app/mailers/customer_text.rb
@@ -42,4 +42,46 @@ class CustomerText
     to: to,
     body: "A job in Lien Flash desperately requires your attention.")
   end
+  
+  def test_job_creation
+    account_sid = ENV['TEST_TWILIO_SID']
+    auth_token = ENV['TEST_TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+14159413974' # Your Twilio number
+    to = '+17203192655' # Your mobile phone number
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "A new job has been created using Lien Flash.")
+  end
+
+  def test_job_text_notification
+    account_sid = ENV['TEST_TWILIO_SID']
+    auth_token = ENV['TEST_TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+14159413974' # Your Twilio number
+    to = '+17203192655' # Your mobile phone number
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "A job in Lien Flash requires your attention.")
+  end
+
+  def test_final_text_notification
+    account_sid = ENV['TEST_TWILIO_SID']
+    auth_token = ENV['TEST_TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+14159413974' # Your Twilio number
+    to = '+17203192655' # Your mobile phone number
+
+    client.messages.create(
+    from: from,
+    to: to,
+    body: "A job in Lien Flash desperately requires your attention.")
+  end
 end
