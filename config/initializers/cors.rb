564e0c4bcf7722e2d8a4c3bd96a5a48a35dd2031
Nick Edwin <nickedwin85@gmail.com>
2020-10-30 10:15:14 -0600

    Cors error still... reverting back

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://lienflash-be.herokuapp.com/'
 
     resource '*',
       headers: :any,
