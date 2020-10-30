2e41c07e14c128db1636383288edc9b90019f2b6
Nick Edwin <nickedwin85@gmail.com>
2020-10-30 11:11:59 -0600

    Another try at cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,15 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'https://lienflash-be.herokuapp.com/'
+    origins '*'
+
+    # resource '/cors',
+    #   :headers => :any,
+    #   :methods => [:post],
+    #   :max_age => 0
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      :headers => :any,
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :max_age => 0
   end
 end
