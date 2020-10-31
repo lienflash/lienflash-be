dc115a0fabe71d703cf047e5daf16e9664b20287
Lito White <leland.white@gmail.com>
2020-10-31 10:49:23 -0600

    instance methods test complete

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -21,35 +21,113 @@ RSpec.describe Job, type: :model do
 
   describe 'instance methods' do
     it "default status: 'good standing' if none provided" do
-      job1 = build(:job, status: "")
-      expect(job1).to be_a(Job)
-      expect(job1.status).to be_nil
-      job1.save
-      expect(job1.status).to eq("good standing")
-    end
+    job1 = build(:job, status: "")
+    expect(job1).to be_a(Job)
+    expect(job1.status).to be_nil
+    job1.save
+    expect(job1.status).to eq("good standing")
+  end
 
-    it "default_date: today if not provided" do
-      fake_date_time = DateTime.now
-      allow(DateTime).to receive(:now) {fake_date_time}
-      job1 = build(:job, completion_date: "")
-      expect(job1.completion_date).to be_nil
-      job1.save
-      expect(job1.completion_date).to be_truthy
-      expect(job1.completion_date).to eq(DateTime.now)
-    end
+  it "default_date: today if not provided" do
+    fake_date_time = DateTime.now
+    allow(DateTime).to receive(:now) {fake_date_time}
+    job1 = build(:job, completion_date: "")
+    expect(job1.completion_date).to be_nil
+    job1.save
+    expect(job1.completion_date).to be_truthy
+    expect(job1.completion_date).to eq(DateTime.now)
+  end
 
-    it "days_outstanding" do
-      job = create(:job, completion_date: 1.days.ago)
-      expect(job.days_outstanding).to eq(1)
-    end
+  it "days_outstanding" do
+    job = create(:job, completion_date: 1.days.ago)
+    expect(job.days_outstanding).to eq(1)
+  end
 
-    it "late?" do
-      job = create(:job)
-      job2 = create(:job, completion_date: 1.days.ago)
-      expect(job.days_outstanding).to eq(45)
-      expect(job.late?).to be true
-      expect(job2.late?).to be false
-    end
+  it "late?" do
+    job = create(:job, completion_date: 44.days.ago)
+    job2 = create(:job, completion_date: 1.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 29.days.ago)
+    expect(job.days_outstanding).to eq(44)
+    expect(job.late?).to be false
+    expect(job2.late?).to be false
+    expect(job_labor.late?).to be false
+    travel(1.day)
+    expect(job.late?).to be true
+    expect(job2.late?).to be false
+    expect(job_labor.late?).to be true
+    travel(1.day)
+    expect(job.late?).to be true
+    expect(job2.late?).to be false
+    expect(job_labor.late?).to be true
+  end
+
+  it "second notice?" do
+    job_tm = create(:job, completion_date: 74.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 44.days.ago)
+    expect(job_tm.second_notice?).to be false
+    expect(job_labor.second_notice?).to be false
+    travel(1.day)
+    expect(job_tm.second_notice?).to be true
+    expect(job_labor.second_notice?).to be true
+    travel(1.day)
+    expect(job_tm.second_notice?).to be false
+    expect(job_labor.second_notice?).to be false
+  end
+
+  it "third notice?" do
+    job_tm = create(:job, completion_date: 89.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 89.days.ago)
+    expect(job_tm.third_notice?).to be false
+    expect(job_labor.third_notice?).to be nil
+    travel(1.day)
+    expect(job_tm.third_notice?).to be true
+    expect(job_labor.third_notice?).to be nil
+    travel(1.day)
+    expect(job_tm.third_notice?).to be false
+    expect(job_labor.third_notice?).to be nil
+  end
+
+  it "fourth notice?" do
+    job_tm = create(:job, completion_date: 99.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 99.days.ago)
+    expect(job_tm.fourth_notice?).to be false
+    expect(job_labor.fourth_notice?).to be nil
+    travel(1.day)
+    expect(job_tm.fourth_notice?).to be true
+    expect(job_labor.fourth_notice?).to be nil
+    travel(1.day)
+    expect(job_tm.fourth_notice?).to be false
+    expect(job_labor.fourth_notice?).to be nil
+  end
+
+  it "final notice?" do
+    job_tm = create(:job, completion_date: 104.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 46.days.ago)
+    expect(job_tm.final_notice?).to be false
+    expect(job_labor.final_notice?).to be false
+    travel(1.day)
+    expect(job_tm.final_notice?).to be true
+    expect(job_labor.final_notice?).to be true
+    travel(1.day)
+    expect(job_tm.final_notice?).to be false
+    expect(job_labor.final_notice?).to be false
+  end
+
+  it "expired?" do
+    job_tm = create(:job, completion_date: 109.days.ago)
+    job_labor = create(:job, job_type: "Labor", completion_date: 49.days.ago)
+    expect(job_tm.expired?).to be false
+    expect(job_labor.expired?).to be false
+    travel(1.day)
+    expect(job_tm.expired?).to be true
+    expect(job_labor.expired?).to be true
+    travel(1.day)
+    expect(job_tm.expired?).to be true
+    expect(job_labor.expired?).to be true
+    travel(1.day)
+    expect(job_tm.expired?).to be true
+    expect(job_labor.expired?).to be true
+  end
 
     it "status_update for job_type 'Materials & Labor'" do
       job1 = create(:job, completion_date: 44.days.ago)
@@ -65,7 +143,7 @@ RSpec.describe Job, type: :model do
       job2.status_update
       job1 = Job.first
       job2 = Job.last
-      
+
       expect(job1.status).to eq("NOI Eligible")
       expect(job2.status).to eq("NOI filed")
 
@@ -76,7 +154,7 @@ RSpec.describe Job, type: :model do
       job1 = Job.first
       job2 = Job.last
       # We are hitting the right conditional but need to test the notifications
-      
+
       #testing 3rd notice
       travel(15.day)
       job1.status_update
@@ -84,7 +162,7 @@ RSpec.describe Job, type: :model do
       job1 = Job.first
       job2 = Job.last
       # We are hitting the right conditional but need to test the notifications
-      
+
       #testing 4th notice
       travel(10.day)
       job1.status_update
@@ -106,7 +184,7 @@ RSpec.describe Job, type: :model do
       job2.status_update
       job1 = Job.first
       job2 = Job.last
-      
+
       expect(job2.status).to eq("NOI filed")
       expect(job1.status).to eq("inactive")
     end
@@ -124,7 +202,7 @@ RSpec.describe Job, type: :model do
       job2.status_update
       job1 = Job.first
       job2 = Job.last
-      
+
       expect(job1.status).to eq("NOI Eligible")
       expect(job2.status).to eq("NOI filed")
 
@@ -135,7 +213,7 @@ RSpec.describe Job, type: :model do
       job1 = Job.first
       job2 = Job.last
       # We are hitting the right conditional but need to test the notifications
-      
+
       #testing final notice
       travel(2.day)
       job1.status_update
@@ -143,7 +221,7 @@ RSpec.describe Job, type: :model do
       job1 = Job.first
       job2 = Job.last
       # We are hitting the right conditional but need to test the notifications
-      
+
       #testing expired?
       travel(3.day)
       job1.status_update
@@ -153,7 +231,7 @@ RSpec.describe Job, type: :model do
       # We are hitting the right conditional but need to test the notifications
       expect(job1.status).to eq("inactive")
       expect(job2.status).to eq("NOI filed")
-      
+
     end
   end
 end
