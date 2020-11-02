3fb6c1ee5b8c666c5c102c32ec429c73cd0119fc
Josh Tukman <josh.tukman@gmail.com>
2020-11-02 10:37:12 -0700

    Uncomment block in cors to fix bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, lo
   allow do
     origins '*'
 
-    # resource '/cors',
-    #   :headers => :any,
-    #   :methods => [:post],
-    #   :max_age => 0
+    resource '/cors',
+      :headers => :any,
+      :methods => [:post],
+      :max_age => 0
 
     resource '*',
       :headers => :any,
