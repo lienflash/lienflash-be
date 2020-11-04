cd7c362edc738aa7cd2bb259870bcd99666b5a50
Josh Tukman <josh.tukman@gmail.com>
2020-11-04 10:12:06 -0700

    Add token attribute to Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,8 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
 
   attributes :name, :business_name, :email, :business_work_number, :business_cell_number, :business_address, :business_address_line2, :business_city, :business_state, :business_zip_code
+  
+  attribute :token do |user, params|
+    params[:token]
+  end
 end
