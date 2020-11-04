905e3703ea8d4585053241a8dff37b9ba9af4932
Josh Tukman <josh.tukman@gmail.com>
2020-11-04 10:12:41 -0700

    Remove Sidekiq configuration

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module LienflashBe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.active_job.queue_adapter = :sidekiq
     config.time_zone = 'Mountain Time (US & Canada)'
     config.active_record.default_timezone = :local
+    #config.middleware.use ActionDispatch::Cookies
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
