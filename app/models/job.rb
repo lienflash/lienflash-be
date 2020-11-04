898f5e5068bb8e7791367ef26d378d00ba81103d
Josh Tukman <josh.tukman@gmail.com>
2020-11-04 10:11:29 -0700

    Removed commented out validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,25 +3,15 @@ class Job < ApplicationRecord
   before_validation :default_date, :default_status
 
   validates :job_type, presence: true
-  validates :job_site_contact_name, presence: true
-  # validates :job_site_name
-  validates :job_site_address, presence: true
-  # validates :job_site_address_line_2
+  validates :job_site_contact_name, presence: true 
+  validates :job_site_address, presence: true 
   validates :job_site_city, presence: true
   validates :job_site_state, presence: true
   validates :job_site_zip_code, presence: true
   validates :completion_date, presence: true
   validates :description_of_work, presence: true
-  #validates :labor_cost
-  #validates :material_cost
   validates :total_cost, presence: true
-  # validates :client_company_name
-  # validates :business_address
-  # validates :business_address_line_2
-  # validates :business_city
-  # validates :business_state
-  # validates :business_zip_code
-  # validates :additional_info
+
   enum status: { "Good Standing": 0, "NOI Eligible": 1, "NOI Requested": 2, "NOI Filed": 3, "Lien Filed": 4, "Inactive": 5}
 
   belongs_to :user
@@ -96,7 +86,7 @@ class Job < ApplicationRecord
     job = Job.find(self.id)
     if job.late? && job.status == "Good Standing"
       job.status = 1
-    elsif job.expired? && job.status_of_NOI_eligible?
+    elsif job.expired? && job.status != "NOI Filed"
       job.status = 5
     end
     job.save
@@ -104,10 +94,13 @@ class Job < ApplicationRecord
 
   def notifications
     job = Job.find(self.id)
-    # user = User.where(user_id: job.user_id)
+    user = User.find(job.user_id)
     if job.status_of_NOI_eligible? && job.first_notice?
-      # UserNotifierMailer.send_first_notice_email(job, user).deliver_now if job.job_type == "Materials & Labor"
-      # UserNotifierMailer.send_just_labor_first_notice_email(job, user).deliver_now if job.job_type == "Labor"
+      if job.job_type == "Materials & Labor"
+        UserNotifierMailer.send_first_notice_email(job, user).deliver_now
+      else
+        UserNotifierMailer.send_just_labor_first_notice_email(job, user).deliver_now
+      end
       # CustomerText.new.job_text_notification
     elsif job.status_of_NOI_eligible? && job.second_notice?
       # UserNotifierMailer.send_second_notice_email(job, user).deliver_now if job.job_type == "Materials & Labor"
