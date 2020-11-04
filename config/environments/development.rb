546c593fdaafffb08dbee394b99edf2876f7f3f2
Josh Tukman <josh.tukman@gmail.com>
2020-11-04 10:13:45 -0700

    Add mailcatcher configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -30,7 +30,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
