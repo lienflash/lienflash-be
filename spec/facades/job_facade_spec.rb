c31578896166ac0e1afa6b7dfdd92d3a5a9c1a13
Josh Tukman <josh.tukman@gmail.com>
2020-11-04 20:55:34 -0700

    Fix test files to utilize the new user relationship

--- spec/facades/job_facade_spec.rb
+++ spec/facades/job_facade_spec.rb
@@ -5,7 +5,7 @@ require 'date'
 RSpec.describe  "Rake Tasks" do
   describe 'Job Facade' do
     it 'can update the status of all jobs' do
-      user1 = create(:user)
+      user1 = create(:user, email: "josh.tukman@gmail.com", business_cell_number: "+17203192655")
       job1 = create(:job, completion_date: 44.days.ago, user_id: user1.id)
       job2 = create(:job, completion_date: 1.days.ago, user_id: user1.id)
       job3 = create(:job, status: "NOI Filed", completion_date: 44.days.ago, user_id: user1.id)
@@ -20,7 +20,8 @@ RSpec.describe  "Rake Tasks" do
 
       job1 = Job.first
       job3 = Job.last
-
+      job2 = Job.find(job2.id)
+      
       expect(job1.status).to eq("NOI Eligible")
       expect(job2.status).to eq("Good Standing")
       expect(job3.status).to eq("NOI Filed")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :business_name }
     it { should validate_presence_of :email }
-    # it { should validate_presence_of :business_work_number }
     it { should validate_presence_of :business_cell_number }
     it { should validate_presence_of :business_address }
-    # it { should validate_presence_of :business_address_line2 }
     it { should validate_presence_of :business_city }
     it { should validate_presence_of :business_state }
     it { should validate_presence_of :business_zip_code }
