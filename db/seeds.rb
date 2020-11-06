b01955920317f545a73748c44167c45c981aa77c
Nick Edwin <nickedwin85@gmail.com>
2020-11-05 19:58:35 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ user1 =  User.create(
           business_name: "Timmy's plumbing",
           email: "josh.tukman@gmail.com",
           business_work_number: "555-123-4567",
-          business_cell_number: "7203192655",
+          business_cell_number: "+17203192655",
           business_address: "123 Main St.",
           business_city: "Denver",
           business_state: "CO",
