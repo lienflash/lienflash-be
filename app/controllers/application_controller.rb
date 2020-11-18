078da2c2e19a36f619bf3f826eac89e53ab2d2af
Josh Tukman <josh.tukman@gmail.com>
2020-11-18 12:49:41 -0700

    Change JWT secret key to one specific to Rails app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :authorized
   def encode_token(payload)
-    JWT.encode(payload, 's3cr3t')
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   def auth_header
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
       token = auth_header.split(' ')[1]
       # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
+        JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
