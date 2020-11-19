b347385964043ae6e5a2445d8a5bb8579ed3657f
Josh Tukman <josh.tukman@gmail.com>
2020-11-19 14:55:42 -0700

    New namespaced users controller for admin

--- app/controllers/api/v1/admin/users_controller.rb
+++ app/controllers/api/v1/admin/users_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::Admin::UsersController < Api::V1::Admin::BaseController
+  before_action :admin? && :authorized
   def index
-    @users = User.all
+    users = User.all
+    render json: UserSerializer.new(users), status: 200
   end
   
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    render json: UserSerializer.new(user), status: 200
+  end
+
+  def delete
+    user = User.find(params[:id])
+    user.delete
+    render status: 204
   end
 end
