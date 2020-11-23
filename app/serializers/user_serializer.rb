3b6e05006fd65dc69e6af6c78646c1a989891239
Josh Tukman <josh.tukman@gmail.com>
2020-11-23 14:09:58 -0700

    Add role to json output in UsersSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :name, :business_name, :email, :business_work_number, :business_cell_number, :business_address, :business_address_line2, :business_city, :business_state, :business_zip_code
+  attributes :name, :business_name, :email, :business_work_number, :business_cell_number, :business_address, :business_address_line2, :business_city, :business_state, :business_zip_code, :role
   
   attribute :token do |user, params|
     params[:token]
