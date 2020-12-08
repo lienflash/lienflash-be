55a334e717360fc794b565314c24cbda73dbeb40
Nick Edwin <nickedwin85@gmail.com>
2020-12-07 17:21:17 -0700

    Updated job object to also return its associated users name

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -2,4 +2,8 @@ class JobSerializer
   include FastJsonapi::ObjectSerializer
 
   attributes :job_type, :job_site_contact_name, :job_site_name, :job_site_address, :job_site_address_line_2, :job_site_city, :job_site_state, :job_site_zip_code, :completion_date, :description_of_work, :labor_cost, :material_cost, :total_cost, :client_company_name, :business_address, :business_address_line_2, :business_city, :business_state, :business_zip_code, :additional_info, :status, :user_id
+
+  attribute :users_name do |user|
+    User.find(user.user_id).name
+  end
 end
