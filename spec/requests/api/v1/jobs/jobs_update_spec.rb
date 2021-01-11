1b3f46e41dadaefc7978f3adae96695c38a69073
Nick Edwin <nickedwin85@gmail.com>
2021-01-11 14:21:39 -0700

    Updated test for single job update

--- spec/requests/api/v1/jobs/jobs_update_spec.rb
+++ spec/requests/api/v1/jobs/jobs_update_spec.rb
@@ -1,44 +1,62 @@
 require 'rails_helper'
 
-# Rails.describe "API V1 Jovs", type: 'request' do
-#   describe "Patch /api/v1/jobs/:id" do
-#     before(:each) do
-#       @job1 = Job.create(
-#         job_street: "123 Main St.",
-#         job_city: "Denver",
-#         job_state: "Co",
-#         job_zip: "80218",
-#         date_of_completion: "Time.now - 20 ",
-#         company_name: "Construction Co Inc LLC",
-#         contact_name: "Timmy Tim Tim",
-#         material_cost: 1000.00,
-#         labor_cost: 100.00,
-#         job_description: "We fixed some stuff.",
-#         job_id: "ABC123"
-#       )
-#       @job2 = Job.create(
-#         job_street: "1 Different Rd.",
-#         job_city: "Denver",
-#         job_state: "Co",
-#         job_zip: "80218",
-#         date_of_completion: "2020-01-01T04:05:06+00:00",
-#         company_name: "Construction Co Inc LLC",
-#         contact_name: "Timmy Tim Tim",
-#         material_cost: 1000.00,
-#         labor_cost: 100.00,
-#         job_description: "We fixed some stuff.",
-#         job_id: "ABC123"
-#       )
-#     end
-#     it "can update the status of a job" do
-#       patch "/api/v1/jobs/#{@job1.id}"
+RSpec.describe "As a  user" do
+  describe "job show" do
+    before(:each) do
+      @user1 =  User.create(
+          name: "Timmy",
+          business_name: "Joshy's plumbing",
+          email: "josh.tukman@gmail.com",
+          business_work_number: "555-123-4567",
+          business_cell_number: "+17203192655",
+          business_address: "123 Main St.",
+          business_city: "Denver",
+          business_state: "CO",
+          business_zip_code: "80218",
+          password: "password1",
+          password_confirmation: "password1",
+          role: 1
+        )
+      allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
+    end
 
-#       expect(response).to be_successful
-#       expect(response.status).to eq(200)
+    it "can inactive a job" do
+      create_list(:job, 3, user_id: @user1.id)
+      @job = Job.last
+      expect(@job.status).to eq("Good Standing")
 
-#       json = JSON.parse(response.body, symbolize_names: true)
+      patch "/api/v1/users/#{@user1.id}/jobs/#{@job.id}?status=5"
 
-#       expect(json[:data][:attributes][:status]).to eq("NOI Eligible")
-#     end
-#   end
-# end
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:attributes][:status]).to eq("Inactive")
+    end
+
+    it "can change status to NOI Requested" do
+      create_list(:job, 3, user_id: @user1.id)
+      @job = Job.last
+      expect(@job.status).to eq("Good Standing")
+
+      patch "/api/v1/users/#{@user1.id}/jobs/#{@job.id}?status=2"
+
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:attributes][:status]).to eq("NOI Requested")
+    end
+
+    it "can check status update against method" do
+      create_list(:job, 3, user_id: @user1.id)
+      @job = Job.last
+      expect(@job.status).to eq("Good Standing")
+
+      patch "/api/v1/users/#{@user1.id}/jobs/#{@job.id}?status=1"
+
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:attributes][:status]).to eq("Good Standing")
+    end
+  end
+end
